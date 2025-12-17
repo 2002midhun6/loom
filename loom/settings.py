2dfdd1f99621542a4fe3616de5fb59ba0fdd7d3e
Midhun <midhung@Midhuns-MacBook-Air.local>
2025-12-17 19:04:21 +0530

    allowed host changed

--- loom/settings.py
+++ loom/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'django-insecure-5p**#d(!l6og@ux_ys#kd4=o4g(szkf&msib$d2=bso*c@4yi5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.233.115.158']
+ALLOWED_HOSTS = [
+    '13.233.115.158',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
@@ -137,17 +141,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
+# STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Additional directories with static files
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR / "static")
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR / "static")
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
